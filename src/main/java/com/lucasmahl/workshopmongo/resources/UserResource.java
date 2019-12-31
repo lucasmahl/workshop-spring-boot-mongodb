fa5ca777be7c131af371308b7b453635d19bbd78
lucasmahl <lucasmahl@gmail.com>
2019-12-31 12:24:31 -0300

    Usando padrao DTO pra retornar usuarios

--- src/main/java/com/lucasmahl/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasmahl/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lucasmahl.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasmahl.workshopmongo.domain.User;
+import com.lucasmahl.workshopmongo.dto.UserDTO;
 import com.lucasmahl.workshopmongo.services.UserService;
 
 //esta classe será um recurso rest (controlador rest), irá conversar com o serviço
 @RestController
 @RequestMapping(value = "/users") // caminho do endpoint
 public class UserResource {
-	
-	//injetando o serviço
+
+	// injetando o serviço
 	@Autowired
 	private UserService service;
 
@@ -24,19 +26,22 @@ public class UserResource {
 	// endpoint do caminho /users, declarado acima
 	// ou só @GetMapping, q dá na mesma
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {// metodos q irá retornar uma lista de usuário
+	public ResponseEntity<List<UserDTO>> findAll() {// metodos q irá retornar uma lista de usuário
 		// ResponseEntity, objeto q irá encapsular toda estrutura necessária pra
 		// retornar resposta http
 		// com possiveis cabeçalhos e possiveis erros
 
-		
+		List<User> list = service.findAll();// lista recebe o service
 
-		List<User> list = service.findAll();//lista recebe o service
+		// converte a lista de User pra lista de UserDTO
+		List<UserDTO> listDto = list.stream()// transforma em uma stream q é uma coleção compativel com expressões lambda
+				.map(x -> new UserDTO(x)) // pra cada objeto irá retornar um new UserDTO
+				.collect(Collectors.toList()); // volta o stream pra List
 
-		//list.addAll(Arrays.asList(maria, alex));// com Arrays.asList posso adicionar tudo de uma vez
+		// list.addAll(Arrays.asList(maria, alex));// com Arrays.asList posso adicionar tudo de uma vez
 
-		return ResponseEntity.ok().body(list);// ok é o metodo q irá instancia o ResponseEntity, já com o código de
+		return ResponseEntity.ok().body(listDto);// ok é o metodo q irá instancia o ResponseEntity, já com o código de
 												// resposta http, de sucesso
-		// body pra definir qual será o corpo da resposta
+												//body pra definir qual será o corpo da resposta
 	}
 }
